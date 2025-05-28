4bbcdae0f3bda90964efc19b02a0b55f1e683a18
RayaanAlb <rayaan@olarm.com>
2025-05-28 12:38:12 +0200

    format dev eui

--- getDEVUID/getDEVUID.py
+++ getDEVUID/getDEVUID.py
@@ -10,6 +10,15 @@ openocd_cmd = (
     f'-c "exit"'
 )
 
+def reverse_and_remove_spaces(uid_string):
+    """Reverse the UID and remove spaces"""
+    # Remove spaces and convert to a list of 2-char hex values
+    hex_values = uid_string.split()
+    # Reverse the order
+    reversed_values = hex_values[::-1]
+    # Join without spaces
+    return ''.join(reversed_values)
+
 # Run the OpenOCD command using pexpect
 child = pexpect.spawn(openocd_cmd, encoding='utf-8', timeout=10)
 # Wait for the command to complete
@@ -24,4 +33,9 @@ for line in lines:
         # Split the line into parts and extract the last 3 chunks
         parts = line.split()[1:]
         devui = parts[1] + parts[0]
-        print("Device UID:", " ".join(devui[i:i+2].upper() for i in range(0, len(devui), 2)))
+        formatted_uid = " ".join(devui[i:i+2].upper() for i in range(0, len(devui), 2))
+        print("Device UID:", formatted_uid)
+        
+        # Print the reversed and space-removed version
+        reversed_uid = reverse_and_remove_spaces(formatted_uid)
+        print("Reversed UID (no spaces):", reversed_uid)
